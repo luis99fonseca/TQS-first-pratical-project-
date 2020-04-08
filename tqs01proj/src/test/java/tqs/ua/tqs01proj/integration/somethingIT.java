b81b8f14536c7bd8ea095c7995b3f9ee536b4e6b
LUISCDF <luiscdf@ua.pt>
2020-04-08 14:45:40 +0100

    mvn plugin for IT

--- tqs01proj/src/test/java/tqs/ua/tqs01proj/integration/somethingIT.java
+++ tqs01proj/src/test/java/tqs/ua/tqs01proj/integration/somethingIT.java
@@ -2,7 +2,7 @@ package tqs.ua.tqs01proj.integration;
 
 import org.junit.jupiter.api.Test;
 
-public class somethingTest {
+public class somethingIT {
 
     // But Continuous is in there too. If the integration tests are taking minutes or hours, it's not continuous. –
     @Test
